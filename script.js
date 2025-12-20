5e1ccf76cfbc02d0521f0bb80e28f75c0d108772
Vincent Ma <vincentma.code@gmail.com>
2025-12-20 03:27:53 -0500

    added function to increment likes

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
+//increment likes
+document.addEventListener('click', function(e){
+    if (e.target.dataset.like){
+        handleLikeClick(e.target.dataset.like)
+    }
+})
+
+function handleLikeClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    } else {
+        targetTweetObj.likes++
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
 function getFeedHtml(){
     let feedHtml = ``
 
@@ -19,15 +39,18 @@ function getFeedHtml(){
                         <p class="tweet-text">${tweet.tweetText}</p>
                         <div class="tweet-details">
                             <span class="tweet-detail">
-                                <i class="fa-regular fa-comment-dots"></i>
+                                <i class="fa-regular fa-comment-dots"
+                                    data-reply="${tweet.uuid}"></i>
                                 ${tweet.replies.length}
                             </span>
                             <span class="tweet-detail">
-                                <i class="fa-solid fa-heart"></i>
+                                <i class="fa-solid fa-heart"
+                                    data-like="${tweet.uuid}"></i>
                                 ${tweet.likes}
                             </span>
                             <span class="tweet-detail">
-                                <i class="fa-solid fa-retweet"></i>
+                                <i class="fa-solid fa-retweet"
+                                    data-retweet="${tweet.uuid}"></i>
                                 ${tweet.retweets}
                             </span>
                         </div>   
